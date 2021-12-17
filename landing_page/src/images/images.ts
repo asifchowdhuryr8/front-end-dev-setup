33b933679493416bced94b6972764ebc1f68b8d9
ITS MAC <asifchowdhuryrafi143@outlook.com>
2021-12-17 16:59:06 +0600

    Add landing page project setup with TypeScript.

---
+++ landing_page/src/images/images.ts
@@ -0,0 +1,8 @@
+import "./mobile/kiwi-mobile.jpg"
+import "./mobile/story-2-mobile.jpeg";
+
+import "./tablet/kiwi-tablet.jpg";
+import "./tablet/story-2-tablet.jpeg";
+
+import "./desktop/kiwi-desktop.jpg"
+import "./desktop/story-2-desktop.jpeg"
---
+++ landing_page/src/typescript/app.ts
@@ -0,0 +1,6 @@
+import "./utilities/inside_another_folder";
+import "./_optional_property";
+
+const appTs: string = "Main TypeScript file"
+
+console.log(appTs);
