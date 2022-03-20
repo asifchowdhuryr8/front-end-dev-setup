da4f13de3894b301fdaa7be8b63fc8cf3b7e02c0
ITS MAC <asifchowdhuryrafi143@outlook.com>
2022-03-20 19:14:47 +0600

    Add header and footer dynamically with JavaScript.
    You can change header and footer once and it changes every page of you website. Effective for multiple pages website. Remove the dynamic option and add it to every page before deployment.

--- landing_page/src/typescript/app.ts
+++ landing_page/src/typescript/app.ts
@@ -1,10 +1,12 @@
 import './utilities/inside_another_folder';
+import './components/header';
+import './components/footer';
 import './_optional_property';
 
 const appTs: string = 'Main TypeScript file';
 console.log(appTs);
 
-// Dynamically Adding class to check is purgecss able to add it to the output
+// Dynamically Adding class to check if purgecss able to add it to the output
 const paragraph = document.querySelector('p');
 paragraph?.addEventListener('click', () => {
     paragraph?.classList.toggle('js-purgecss-test');
---
+++ landing_page/src/typescript/components/footer.ts
@@ -0,0 +1,26 @@
+const footer = document.querySelector('#footer');
+footer?.insertAdjacentHTML(
+    "beforeend",
+    `<ul>
+        <li><a class="footer-link" href="about.html">About</a></li>
+        <li><a class="footer-link" href="work.html">Work</a></li>
+        <li><a class="footer-link" href="contact.html">Contact</a></li>
+    </ul>
+    <ul class="social-row">
+        <li>
+            <a class="footer-link" href="https://github.com/my-github-profile">
+                <i class="fab fa-github"></i>
+            </a>
+        </li>
+        <li>
+            <a class="footer-link" href="https://twitter.com/my-twitter-profile">
+                <i class="fab fa-twitter"></i>
+            </a>
+        </li>
+        <li>
+            <a class="footer-link" href="https://www.linkedin.com/in/my-linkedin-profile">
+                <i class="fab fa-linkedin"></i>
+            </a>
+        </li>
+    </ul>`
+);
