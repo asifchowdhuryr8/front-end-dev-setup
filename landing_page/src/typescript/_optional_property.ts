b0c2f6b942062ba7f8f31c4f4feb42318e7d5452
ITS MAC <asifchowdhuryrafi143@outlook.com>
2022-04-23 13:04:52 +0600

    Clean the folder and implement the 7-1 CSS Architecture with Sass.

--- landing_page/src/typescript/_optional_property.ts
+++
@@ -1,2 +0,0 @@
-const rootFolder: string = "I am from typescript's root folder file"
-console.log(rootFolder);
--- landing_page/src/typescript/app.ts
+++ landing_page/src/typescript/app.ts
@@ -1,10 +1,5 @@
-import './utilities/inside_another_folder';
 import './components/header';
 import './components/footer';
-import './_optional_property';
-
-const appTs: string = 'Main TypeScript file';
-console.log(appTs);
 
 // Dynamically Adding class to check if purgecss able to add it to the output
 const paragraph = document.querySelector('p');
