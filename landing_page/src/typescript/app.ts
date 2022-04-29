eaeec337b8bccf02336dbd975e63126d09f2e363
ITS MAC <asifchowdhuryrafi143@outlook.com>
2022-04-29 11:42:51 +0600

    Ready to start a new project

--- landing_page/src/typescript/app.ts
+++ landing_page/src/typescript/app.ts
@@ -1,8 +1,2 @@
 import './components/header';
 import './components/footer';
-
-// Dynamically Adding class to check if purgecss able to add it to the output
-const paragraph = document.querySelector('p');
-paragraph?.addEventListener('click', () => {
-    paragraph?.classList.toggle('js-purgecss-test');
-});
