9385aa9c4f9cbfbdd481b4110e5fd7613a191402
ITS MAC <asifchowdhuryrafi143@outlook.com>
2023-05-04 23:36:01 +0600

    Update landing page project setup.

--- landing_page/src/typescript/components/_footer.ts
+++ landing_page/src/typescript/components/_footer.ts
@@ -1,26 +1,2 @@
 const footer = document.querySelector('#footer');
-footer?.insertAdjacentHTML(
-    'beforeend',
-    `<ul>
-        <li><a class="footer-link" href="about.html">About</a></li>
-        <li><a class="footer-link" href="work.html">Work</a></li>
-        <li><a class="footer-link" href="contact.html">Contact</a></li>
-    </ul>
-    <ul class="social-row">
-        <li>
-            <a class="footer-link" href="https://github.com/my-github-profile">
-                <i class="fab fa-github"></i>
-            </a>
-        </li>
-        <li>
-            <a class="footer-link" href="https://twitter.com/my-twitter-profile">
-                <i class="fab fa-twitter"></i>
-            </a>
-        </li>
-        <li>
-            <a class="footer-link" href="https://www.linkedin.com/in/my-linkedin-profile">
-                <i class="fab fa-linkedin"></i>
-            </a>
-        </li>
-    </ul>`,
-);
+footer?.insertAdjacentHTML('beforeend', '<div class="container footer"><p class=m-0>Developed by ITS MAC<ul class=social><li class=social__item><a class=social__link href=https://www.linkedin.com/in/itsmacr8><i class="fab fa-linkedin"></i></a><li class=social__item><a class=social__link href=https://github.com/itsmacr8><i class="fab fa-github"></i></a><li class=social__item><a class=social__link href=https://twitter.com/itsmacr8><i class="fab fa-twitter"></i></a></ul></div>');
---
+++ landing_page/src/typescript/components/_sticky_navigation.ts
@@ -0,0 +1,50 @@
+const nav: HTMLElement = document.querySelector('nav') as HTMLElement;
+const supportPageOffset: boolean = window.pageXOffset !== undefined;
+const isCSS1Compat: boolean = (document.compatMode || '') === 'CSS1Compat';
+
+function getScrolledPosition(): number {
+    const CSS1Compat = isCSS1Compat
+        ? document.documentElement.scrollTop
+        : document.body.scrollTop;
+    return supportPageOffset ? window.pageYOffset : CSS1Compat;
+}
+
+let previousScrollPosition: number = 0;
+const isScrollingDown = (): boolean => {
+    const scrolledPosition = getScrolledPosition();
+    const isScrollDown: boolean = scrolledPosition > previousScrollPosition;
+
+    // Temporarily solution to fix the issue with the sticky navbar when
+    // clicking on the hamburger menu to show the navbar links on tablet and
+    // mobile devices. 100 is a random number and it works fine.
+    previousScrollPosition = scrolledPosition + 100;
+    return isScrollDown;
+};
+
+const handleNavScroll = () => {
+    if (isScrollingDown() && !nav?.contains(document.activeElement)) {
+        nav?.classList.add('scroll-down');
+        nav?.classList.remove('scroll-up');
+    } else {
+        nav?.classList.add('scroll-up');
+        nav?.classList.remove('scroll-down');
+    }
+};
+
+let throttleTimer: boolean;
+const throttle = (callback: Function, time: number) => {
+    if (throttleTimer) return;
+
+    throttleTimer = true;
+    setTimeout(() => {
+        callback();
+        throttleTimer = false;
+    }, time);
+};
+
+const mediaQuery = window.matchMedia('(prefers-reduced-motion: reduce)');
+window.addEventListener('scroll', () => {
+    if (mediaQuery && !mediaQuery.matches) {
+        throttle(handleNavScroll, 750);
+    }
+});
